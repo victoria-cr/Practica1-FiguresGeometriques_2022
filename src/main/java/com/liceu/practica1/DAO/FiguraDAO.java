28f15be8a30aa92ff9a40e8c4af044ec603e2e4b
victoriacr <victoriacalvrodri@gmail.com>
2022-11-15 16:14:22 +0100

    java

--- src/main/java/com/liceu/practica1/DAO/FiguraDAO.java
+++ src/main/java/com/liceu/practica1/DAO/FiguraDAO.java
@@ -2,8 +2,12 @@ package com.liceu.practica1.DAO;
 
 import com.liceu.practica1.model.Figura;
 
+import java.util.List;
+
 public interface FiguraDAO {
     void guardarFigura(Figura figura);
+
+    List<Figura> getFiguresCreades();
 
     Object llistaPerUsuari(int usuariID);
 
